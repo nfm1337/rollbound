9624c4afaebf2fe2c7ef19bc9f8adf4565441c2a
Daniel Olovianniy <daniel.olovianniy@gmail.com>
2026-09-05 20:32:10 +0300

    init commit

---
+++ android/src/main/kotlin/io/github/nfm1337/android/AndroidLauncher.kt
@@ -0,0 +1,18 @@
+package io.github.nfm1337.android
+
+import android.os.Bundle
+
+import com.badlogic.gdx.backends.android.AndroidApplication
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration
+import io.github.nfm1337.Main
+
+/** Launches the Android application. */
+class AndroidLauncher : AndroidApplication() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        initialize(Main(), AndroidApplicationConfiguration().apply {
+            // Configure your application here.
+            useImmersiveMode = true // Recommended, but not required.
+        })
+    }
+}
